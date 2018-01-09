b48109c6c1196fa7cacd930bb1f53bfe3e13127f
Applemelon <alexlolol2011@live.dk>
2018-01-09 11:13:42 +0100

    Description view is a thing now

--- PMC/src/pmc/PMC.java
+++ PMC/src/pmc/PMC.java
@@ -27,6 +27,7 @@ public class PMC extends Application {
     @Override
     public void init() throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("gui/view/MainWindowView.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("gui/view/MovieDetailsView.fxml"));
         
         scene = new Scene(root);
         
