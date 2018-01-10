0a8f1da13ecd724a9e9ab0c67ebcfc0a876b2073
Applemelon <alexlolol2011@live.dk>
2018-01-10 10:17:59 +0100

    LOTSA stuff

--- PMC/src/pmc/gui/model/MovieDetailsModel.java
+++ PMC/src/pmc/gui/model/MovieDetailsModel.java
@@ -16,6 +16,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 
 /**
  *
@@ -26,26 +28,50 @@ public class MovieDetailsModel {
     public void initialize(URL url, ResourceBundle rb) {
     }
 
-    public void setImage(ImageView imageMoviePoster) {
-        File file = new File("Movies/timmy.jpg");
+    public void setPosterImage(ImageView imageMoviePoster) {
+        File file = new File("src/pmc/gui/resources/timmy.jpg");
+        Image image = new Image(file.toURI().toString());
+        imageMoviePoster.setImage(image);
+    }
+    
+    public void setRatingImage(ImageView imageMoviePoster) {
+        File file = new File("src/pmc/gui/resources/star.png");
         Image image = new Image(file.toURI().toString());
         imageMoviePoster.setImage(image);
     }
 
-//    public void setTitleAndYear(Label lblTitleAndYear) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    public void setDescription(TextArea textareaDescription) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    public void setGenres(Label lblGenres) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    public void setDirector(Label lblDirector) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
+    public void setTitleAndYear(Label lblTitleAndYear) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setDescription(TextArea textareaDescription) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setGenres(Label lblGenres) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setDirector(Label lblDirector) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public void setScore(Label lblScore) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void playMovie() throws IOException {
+        String currentDir = System.getProperty("user.dir") + File.separator;
+        File dir = new File(currentDir);
+        
+        Desktop.getDesktop().open(new File(dir + "\\Movies\\Guy runs into wall.mp4"));
+    }
+
+    public void setClipboard() {
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+        ClipboardContent content = new ClipboardContent();
+        content.putString("google.com");
+        clipboard.setContent(content);
+    }
     
 }
