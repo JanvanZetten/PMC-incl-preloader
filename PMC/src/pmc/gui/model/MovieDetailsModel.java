5799bd068c2949e2b12c55dff219e96ea81a5c0e
Applemelon <alexlolol2011@live.dk>
2018-01-10 13:07:41 +0100

    sqllll

--- PMC/src/pmc/gui/model/MovieDetailsModel.java
+++ PMC/src/pmc/gui/model/MovieDetailsModel.java
@@ -10,8 +10,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
@@ -28,38 +27,51 @@ public class MovieDetailsModel {
     public void initialize(URL url, ResourceBundle rb) {
     }
 
+    //Removes the "Copy IMBd link to clipboard" button if no IMDb link is present.
+    public void determineIMDbLink(Button btnCopyLink) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    //Sets the image of the movie poster.
     public void setPosterImage(ImageView imageMoviePoster) {
         File file = new File("src/pmc/gui/resources/timmy.jpg");
         Image image = new Image(file.toURI().toString());
         imageMoviePoster.setImage(image);
     }
     
+    //Sets the image of the rating star.
     public void setRatingImage(ImageView imageMoviePoster) {
         File file = new File("src/pmc/gui/resources/star.png");
         Image image = new Image(file.toURI().toString());
         imageMoviePoster.setImage(image);
     }
 
+    //Sets the title and year of the movie in one string.
     public void setTitleAndYear(Label lblTitleAndYear) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    //Sets the description of the movie in the description text area.
     public void setDescription(TextArea textareaDescription) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    //Sets the different genres associated with the movie.
     public void setGenres(Label lblGenres) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    //Sets the director of the movie.
     public void setDirector(Label lblDirector) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    //Sets what score the movie has.
     public void setScore(Label lblScore) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    //Plays the movie upon pressing the "Watch Movie" button.
     public void playMovie() throws IOException {
         String currentDir = System.getProperty("user.dir") + File.separator;
         File dir = new File(currentDir);
@@ -67,6 +79,7 @@ public class MovieDetailsModel {
         Desktop.getDesktop().open(new File(dir + "\\Movies\\Guy runs into wall.mp4"));
     }
 
+    //Sets the user's clipboard to contain the IMDb link.
     public void setClipboard() {
         Clipboard clipboard = Clipboard.getSystemClipboard();
         ClipboardContent content = new ClipboardContent();
