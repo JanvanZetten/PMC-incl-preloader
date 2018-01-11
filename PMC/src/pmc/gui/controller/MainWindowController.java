f415d465615c0008698642675743c58d12db7457
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-01-11 09:58:44 +0100

    Refactoring

--- PMC/src/pmc/gui/controller/MainWindowController.java
+++ PMC/src/pmc/gui/controller/MainWindowController.java
@@ -7,29 +7,18 @@ package pmc.gui.controller;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
-import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import pmc.be.Genre;
 import pmc.be.Movie;
 import pmc.gui.model.MainModel;
 
@@ -72,98 +61,7 @@ public class MainWindowController implements Initializable
     {
         mainModel.changeMenubarForMac(menubar, stackPaneFiltering, stackPaneMovieView);
         //initializeTableView();
-        mainModel.initializeTableView(tblviewMovies,tblcolTitle,tblcolGenre,tblcolTime,tblcolImdbRating,tblcolPersonalRating);
-    }
-
-    /**
-     * Setup TableView.
-     */
-    private void initializeTableView()
-    {
-        // Set values for Table Cells.
-        tblcolTitle.setCellValueFactory(new PropertyValueFactory("name"));
-        tblcolGenre.setCellValueFactory((TableColumn.CellDataFeatures<Movie, String> param) ->
-        {
-            List<Genre> gs = param.getValue().getGenres();
-            String txt = "";
-            if (gs != null)
-            {
-                for (Genre g : gs)
-                {
-                    if (txt.equalsIgnoreCase(""))
-                    {
-                        txt = g.getName();
-                    }
-                    else
-                    {
-                        txt += ", " + g.getName();
-                    }
-                }
-            }
-            return new ReadOnlyObjectWrapper<>(txt);
-        });
-        tblcolTime.setCellValueFactory((TableColumn.CellDataFeatures<Movie, String> param) ->
-        {
-            int duration = param.getValue().getDuration();
-            int min = duration % 60;
-            int hour = (duration - min) / 60;
-            if (min < 10)
-            {
-                return new ReadOnlyObjectWrapper<>(hour + "t 0" + min + "min");
-            }
-            return new ReadOnlyObjectWrapper<>(hour + "t " + min + "min");
-        });
-        tblcolTime.setStyle("-fx-alignment: CENTER-RIGHT;");
-        tblcolImdbRating.setCellValueFactory(new PropertyValueFactory("imdbRating"));
-        tblcolImdbRating.setStyle("-fx-alignment: CENTER;");
-        tblcolPersonalRating.setCellValueFactory(new PropertyValueFactory("personalRating"));
-        tblcolPersonalRating.setStyle("-fx-alignment: CENTER;");
-
-        // Set doubleclick on row.
-        tblviewMovies.setRowFactory(tv ->
-        {
-            TableRow<Movie> row = new TableRow<>();
-            row.setOnMouseClicked(event ->
-            {
-                if (event.getClickCount() == 2 && (!row.isEmpty()))
-                {
-                    Movie currentMovie = row.getItem();
-                    mainModel.setCurrentMovie(currentMovie);
-                    handleMovieDetails();
-                }
-                else if (event.getClickCount() == 1 && (!row.isEmpty()))
-                {
-                    mainModel.setCurrentMovie(row.getItem());
-                }
-            });
-            return row;
-        });
-
-        // Add test Movie.
-        mainModel.addMovieToObsLst(new Movie("http://www.imdb.com/title/tt0468569/", "pmc/Movies/Guy runs into wall.mp4"));
-        mainModel.addMovieToObsLst(new Movie("http://www.imdb.com/title/tt1345836/", "pmc/Movies/Guy runs into wall.mp4"));
-        mainModel.addMovieToObsLst(new Movie("http://www.imdb.com/title/tt0097965/", "pmc/Movies/Guy runs into wall.mp4"));
-        mainModel.addMovieToObsLst(new Movie("http://www.imdb.com/title/tt1570728/?ref_=nv_sr_1", "pmc/Movies/Guy runs into wall.mp4"));
-
-        // Set Observable List.
-        tblviewMovies.setItems(mainModel.getFilteredMovies());
-    }
-
-    private void handleMovieDetails()
-    {
-        try
-        {
-            startModalWindow("MovieDetailsView", 620, 394);
-            
-            FXMLLoader fxLoader = new FXMLLoader(getClass().getResource("MovieDetailsView.fxml"));
-            MovieDetailsController cont = new FXMLLoader().getController();
-            
-            cont.setBLLManager(mainModel.getBLLManager());
-        }
-        catch (IOException ex)
-        {
-            throw new RuntimeException("Could not open window!");
-        }
+        mainModel.initializeTableView(tblviewMovies, tblcolTitle, tblcolGenre, tblcolTime, tblcolImdbRating, tblcolPersonalRating);
     }
 
     @FXML
@@ -192,31 +90,12 @@ public class MainWindowController implements Initializable
     {
         try
         {
-            startModalWindow("About", 330, 310);
+            mainModel.startModalWindow("About", 330, 310);
         }
         catch (IOException ex)
         {
             throw new RuntimeException("Could not open window!");
         }
-    }
-
-    /**
-     * Starts a new window by sending in the name of the view in the parameters.
-     */
-    private void startModalWindow(String windowView, int minWidth, int minHeight) throws IOException
-    {
-        Stage newStage = new Stage();
-        newStage.initModality(Modality.APPLICATION_MODAL);
-        FXMLLoader fxLoader = new FXMLLoader(getClass().getResource("/pmc/gui/view/" + windowView + ".fxml"));
-        Parent root = fxLoader.load();
-        Scene scene = new Scene(root);
-
-        newStage.setTitle("PMC - " + windowView);
-        newStage.getIcons().add(new Image("pmc/gui/resources/logo.png"));
-        newStage.setScene(scene);
-        newStage.setMinWidth(minWidth);
-        newStage.setMinHeight(minHeight);
-        newStage.showAndWait();
     }
 
     @FXML
