f9a7ebe2507e34a4752c2d5df706cfd301b17644
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-01-11 09:25:01 +0100

    BLLManager calls for DALManager

---
+++ PMC/src/pmc/bll/BLLException.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pmc.bll;
+
+/**
+ *
+ * @author Asbamz
+ */
+public class BLLException extends Exception
+{
+    public BLLException(String message)
+    {
+        super(message);
+    }
+
+    public BLLException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public BLLException(Throwable cause)
+    {
+        super(cause);
+    }
+
+}
--- PMC/src/pmc/bll/BLLManager.java
+++ PMC/src/pmc/bll/BLLManager.java
@@ -5,27 +5,172 @@
  */
 package pmc.bll;
 
+import java.util.List;
+import pmc.be.Genre;
 import pmc.be.Movie;
+import pmc.dal.DALManager;
+import pmc.dal.DalExeption;
 import pmc.dal.SelectedItemData;
 
 /**
  *
  * @author janvanzetten
  */
-public class BLLManager {
-    
+public class BLLManager
+{
+
+    DALManager dalManager;
     SelectedItemData SID;
-    
-    public BLLManager() {
+
+    public BLLManager()
+    {
+        dalManager = new DALManager();
         SID = new SelectedItemData();
     }
 
-    public void setCurrentMovie(Movie currentMovie) {
+    public void setCurrentMovie(Movie currentMovie)
+    {
         SID.setCurrentMovie(currentMovie);
     }
-    
-    public Movie getCurrentMovie() {
+
+    public Movie getCurrentMovie()
+    {
         return SID.getCurrentMovie();
     }
-    
+
+    /**
+     * gets all the genres
+     * @return list of genre objects
+     * @throws BLLException
+     */
+    public List<Genre> getAllGenres() throws BLLException
+    {
+        try
+        {
+            return dalManager.getAllGenres();
+        }
+        catch (DalExeption ex)
+        {
+            throw new BLLException(ex.getMessage(), ex.getCause());
+        }
+    }
+
+    /**
+     * gets all the movies
+     * @return list of movie objects
+     * @throws BLLException
+     */
+    public List<Movie> getAllMovies() throws BLLException
+    {
+        try
+        {
+            return dalManager.getAllMovies();
+        }
+        catch (DalExeption ex)
+        {
+            throw new BLLException(ex.getMessage(), ex.getCause());
+        }
+    }
+
+    /**
+     * Make a new Genre
+     * @param name the name of the new Genre
+     * @return the newly made genre if it succeeds
+     */
+    public Genre addGenre(String name) throws BLLException
+    {
+        try
+        {
+            return dalManager.addGenre(name);
+        }
+        catch (DalExeption ex)
+        {
+            throw new BLLException(ex.getMessage(), ex.getCause());
+        }
+    }
+
+    /**
+     * Make a new Movie
+     * @param name
+     * @param filePath
+     * @param genres
+     * @param imdbRating
+     * @param personalRating
+     * @param Directors
+     * @param duration
+     * @param ImdbUrl
+     * @param year
+     * @param imageInBytes
+     * @return a Movie object with lastView of -1
+     * @throws BLLException
+     */
+    public Movie addMovie(String name, String filePath, List<Genre> genres,
+            double imdbRating, int personalRating, String Directors,
+            int duration, String ImdbUrl, int year, byte[] imageInBytes) throws BLLException
+    {
+        try
+        {
+            return dalManager.addMovie(name, filePath, genres, imdbRating, personalRating,
+                    Directors, duration, ImdbUrl, year, imageInBytes);
+        }
+        catch (DalExeption ex)
+        {
+            throw new BLLException(ex.getMessage(), ex.getCause());
+        }
+
+    }
+
+    /**
+     * Deletes the given genre. There should be no movies with this genre for it
+     * to be deleted
+     * @param genre the genre to delete
+     * @return true if the genre is deleted
+     * @throws BLLException
+     */
+    public boolean deleteGenre(Genre genre) throws BLLException
+    {
+        try
+        {
+            return dalManager.deleteGenre(genre);
+        }
+        catch (DalExeption ex)
+        {
+            throw new BLLException(ex.getMessage(), ex.getCause());
+        }
+    }
+
+    /**
+     * Deletes the given movie.
+     * @param movie the movie to delete
+     * @return true if movie is deleted
+     * @throws BLLException
+     */
+    public boolean deleteMovie(Movie movie) throws BLLException
+    {
+        try
+        {
+            return dalManager.deleteMovie(movie);
+        }
+        catch (DalExeption ex)
+        {
+            throw new BLLException(ex.getMessage(), ex.getCause());
+        }
+    }
+
+    /**
+     * Updates the movie with the same id as the given movie object
+     * @param updatedMovie should be the updated version with the same id
+     * @throws BLLException
+     */
+    public void updateMovie(Movie updatedMovie) throws BLLException
+    {
+        try
+        {
+            dalManager.updateMovie(updatedMovie);
+        }
+        catch (DalExeption ex)
+        {
+            throw new BLLException(ex.getMessage(), ex.getCause());
+        }
+    }
 }
