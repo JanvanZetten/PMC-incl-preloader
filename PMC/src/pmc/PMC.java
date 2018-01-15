4993e2df841711cc1cce120b5bb877400071f41d
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-01-15 12:51:10 +0100

    Edit window !

--- PMC/src/pmc/PMC.java
+++ PMC/src/pmc/PMC.java
@@ -27,8 +27,8 @@ public class PMC extends Application
     @Override
     public void init() throws Exception
     {
-//        Parent root = FXMLLoader.load(getClass().getResource("gui/view/MainWindowView.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("gui/view/DeletePopupView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("gui/view/MainWindowView.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("gui/view/DeletePopupView.fxml"));
         //Parent root = FXMLLoader.load(getClass().getResource("gui/view/MovieDetailsView.fxml"));
 
         scene = new Scene(root);
